eb40d5c651f8c87228fe1d89ff5f7901e1581e6b
MikesGit138 <michael138leighton@gmail.com>
2022-06-04 20:37:42 -0500

    some styling added

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -4,7 +4,16 @@ const router = express.Router();
 const connection = require('../database/db')
 
 router.get('/leaderboard', (req, res) =>{
-   res.render('leaderboard')
+   let sql = "SELECT * FROM leaderboard.friends"
+   let query = connection.query(sql, (err, rows) =>{
+       if (err) console.log('connection unsuccessful');
+       else console.log('leaderboard query ran')
+   res.render('leaderboard',{
+      table: 'friends',
+      friends: rows
+
+   })
+})
 })
 
 module.exports = router
